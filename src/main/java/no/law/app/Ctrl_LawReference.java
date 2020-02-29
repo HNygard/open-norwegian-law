acbb1fae14a5813e3691699add3ad8f70bfa34b3
Hallvard Nygard <hallvard.nygard@gmail.com>
2020-02-29 13:59:12 +0100

    Redirect to the only part of the app that does anything

--- src/main/java/no/law/app/Ctrl_LawReference.java
+++ src/main/java/no/law/app/Ctrl_LawReference.java
@@ -5,11 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 public class Ctrl_LawReference {
     @GetMapping("/law-reference")
     public String main(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "law-ref-gui";
+    }
+
+    @GetMapping("/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("./law-reference");
     }
 }
